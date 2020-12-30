d1832b98855aa4875a02e8899bf39c8f879b3fdd
Rafael Treiber <rafaeltreiber@gmail.com>
2020-12-30 14:10:20 -0300

    metade

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Content } from './styles';
+import { Container, HeaderWrapper, Header, DropboxLogo, Content } from './styles';
 
 interface Props {
   variant: 'blue' | 'beige' | 'white' | 'black';
@@ -8,8 +8,19 @@ interface Props {
 }
 
 const Section: React.FC<Props> = ({ variant, title, description }) => {
+  const buttonVariant = Math.round(Math.random()); // retorna 0 ou 1
   return(
     <Container className = { variant }>
+      <HeaderWrapper>
+        <Header>
+          <h1>
+            <DropboxLogo />
+            <span>Dropbox</span>
+          </h1>
+
+          <button>{buttonVariant === 0 ? 'Acessar' : 'Interagir'}</button>
+        </Header>
+      </HeaderWrapper>
       <Content>
         <h2>{ title }</h2>
         <p>{ description }</p>
